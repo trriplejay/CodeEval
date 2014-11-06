b4e32b655c97a10046a35f038dd93c457b432e15
trriplejay <stockbauer@gmail.com>
2014-11-06 15:02:07 -0800

    some new exercises

--- python3/fibonacciseries.py
+++ python3/fibonacciseries.py
@@ -31,10 +31,11 @@ class memoize():
 def fib(n):
     """
     calculate the nth fibonacci number
+    via recursion
     """
-    if n==0:
+    if n == 0:
         return 0
-    elif n==1:
+    elif n == 1:
         return 1
     else:
         return fib(n-1) + fib(n-2)
